222df930e091076bd487d6e561a7c40a233bdc23
Aleex1237 <alexis10893@outlook.com>
2022-03-03 20:18:24 -0300

    added create method and fix error catcher

--- controllers/stores.js
+++ controllers/stores.js
@@ -1,18 +1,41 @@
 
-const notesService = require('../services/stores');
-const getAll = async (req,res,next) => {
-    console.log(req);
+const storeService = require('../services/stores');
+const messages = require('../constants/messages');
+const statusCode = require('../constants/statusCodes');
+const logger = require('../utils/logger')
 
-    const notes = await notesService.getAll();
-    res.json(
-    {
-        data: [notes],
-        page: 1,
-        pages: 100,
-        limit: 10,
-        total: 10000,
+const getAll = async (req,res,next) => {
+    try {
+        const {limit, skip} = req.query
+        const data = await storeService.getAll(limit, skip);
+    res.status(statusCode.RESPONSE_OK).json(
+    {   
+        status: statusCode.RESPONSE_OK,
+        msg: messages.RESPONSE_OK,
+        data
     }
     );
+    } catch (error) {
+        next(error);
+    }
+
+    
 }
 
-module.exports = { getAll };
+const create = async (req, res, next) => {
+    try {
+        const store = await storeService.create(req.body);
+
+        logger.info("Store created.")
+        res.status(statusCode.RESPONSE_OK_CREATED).json({
+            status:statusCode.RESPONSE_OK_CREATED,
+            msg:messages.RESPONSE_OK_CREATED,
+            store
+        })
+    } catch (error) {
+        logger.info("Error to create store")
+        next(error);
+    }
+}
+
+module.exports = { getAll, create };
