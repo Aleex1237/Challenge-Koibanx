634ea44188bc6e70b84641af6cbc8a42ed424ef0
Aleex1237 <alexis10893@outlook.com>
2022-03-03 10:30:45 -0300

    added router post for login user

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 
-const auth = require('../middlewares/auth');
 const authController = require('../controllers/auth');
 
-router.post('/', auth.isLogged, authController.login);
+router.post('/', authController.login);
 
 module.exports = router;
