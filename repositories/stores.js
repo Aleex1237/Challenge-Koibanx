6f81598d0889c33fc1bdce9fcbac48e11e6eca9e
Aleex1237 <alexis10893@outlook.com>
2022-03-03 20:26:29 -0300

    added params for pagination, create method getCount and method create

--- repositories/stores.js
+++ repositories/stores.js
@@ -4,10 +4,31 @@ const Store = require('../models/store');
 
 
 
-const getAll = async () => {
-    const notes = await Store.find();
+const getAll = async (limit, skip) => {
+    const notes = await Store.find().limit(limit).skip(skip);
 
     return notes
 }
 
-module.exports = { getAll };
+const getCount = async () => {
+    const count = await Store.find().countDocuments();
+
+    return count
+}
+
+const create = async (data) => {
+    const {name, cuit, currentBalance, lastSale, concepts } = data;
+
+    const note = await Store.create({
+        name,
+        cuit,
+        currentBalance,
+        active: true,
+        lastSale,
+        concepts,
+    })
+
+    return note;
+}
+
+module.exports = { getAll, create, getCount };
