cea1316a8b54f5ed659d3993215379a78348427a
Aleex1237 <alexis10893@outlook.com>
2022-03-03 10:18:48 -0300

    added verifier if is a valid user and he has token

---
+++ middlewares/auth.js
@@ -0,0 +1,32 @@
+const authService = require('../services/auth');
+const messages = require('../constants/messages');
+const statusCode = require('../constants/statusCodes');
+
+const auth = async (req, res, next) => {
+    try {
+        if (!req.headers.authorization) {
+            const error = new Error(messages.NO_TOKEN);
+            error.status = statusCode.FORBIDDEN;
+            throw error;
+        }
+        const token = req.headers.authorization.split(' ')[1];
+    
+        const dataDecoded = Buffer.from(token, 'base64').toString().split(':');
+        console.log(dataDecoded);
+    
+        const user = await authService.getUserById(dataDecoded[0]);
+    
+        if (!user) {
+            const error = new Error(messages.INVALID_TOKEN);
+            error.status = statusCode.FORBIDDEN;
+            return next(error);
+        }
+    
+         next();
+    } catch (error) {
+        next(error);
+    }
+
+}
+
+module.exports = { auth };
