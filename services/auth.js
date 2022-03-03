69c8a18dcb14928fad1156ce959351d8955b793f
Aleex1237 <alexis10893@outlook.com>
2022-03-03 10:29:09 -0300

    fix conditionals and throw errors.

--- services/auth.js
+++ services/auth.js
@@ -1,28 +1,35 @@
-let bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcrypt-nodejs');
+const messages = require('../constants/messages');
+const statusCode = require('../constants/statusCodes');
 
 const usersRepository = require('../repositories/users');
 
 const checkUser = async (email, password) => {
-
+    
     const user = await usersRepository.getByEmail(email);
-
-    if (!user) {
-        return 'invalid credentials';
+    if (user.length === 0) {
+        const error = new Error(messages.EMAIL_NOT_FOUND);
+        error.status = statusCode.NO_USER_CREDENTIALS;
+        throw error;
     }
 
     const comparedPassword = bcrypt.compareSync(password, user[0].password);
 
-    if (!comparedPassword) {
-        return 'invalid credentials'
+    if (comparedPassword !== true) {
+        const error = new Error(messages.NO_MATCH_USER);
+        error.status = statusCode.NO_USER_CREDENTIALS;
+        throw error;
     }
 
-    if (user[0].email !== email && comparedPassword !== true) {
-        return 'invalid credentials'
-    }
-
-    const token = Buffer.from(`${user[0].id}:${user[0].password}`).toString('base64');
+    const token = Buffer.from(`${user[0].id}:${password}`).toString('base64');
 
     return { user, token};
 };
 
-module.exports = { checkUser };
+const getUserById = async (id) => {
+    const user = usersRepository.getById(id);
+
+    return user;
+}
+
+module.exports = { checkUser, getUserById };
