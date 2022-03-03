b2f3acf1e8059f199ae070008413bbb0b52026e0
Aleex1237 <alexis10893@outlook.com>
2022-03-03 10:15:46 -0300

    added messages and status constants

---
+++ constants/messages.js
@@ -0,0 +1,26 @@
+const NOT_FOUND_ERROR = 'Not Found';
+const BAD_REQUEST_ERROR = 'Bad request';
+const INTERNAL_ERROR = 'Internal error';
+const RESPONSE_OK = 'OK';
+const RESPONSE_OK_CREATED = 'Generated successfully';
+const RESPONSE_OK_NO_CONTENT = 'No Content';
+const UNAUTHORIZED_USER_CREDENTIALS = 'Unauthorized user credentials';
+const FORBIDDEN = 'Forbidden';
+const NO_TOKEN = 'There is no token';
+const INVALID_TOKEN = 'The token is invalid';
+const TOKEN_NOT_GENERATED = 'The token couldn\'t be generated';
+
+
+module.exports = {
+  NOT_FOUND_ERROR,
+  BAD_REQUEST_ERROR,
+  INTERNAL_ERROR,
+  RESPONSE_OK,
+  RESPONSE_OK_CREATED,
+  RESPONSE_OK_NO_CONTENT,
+  UNAUTHORIZED_USER_CREDENTIALS,
+  FORBIDDEN,
+  NO_TOKEN,
+  INVALID_TOKEN,
+  TOKEN_NOT_GENERATED,
+};
