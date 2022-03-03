8950dbba0780cc2822ea434c620ec2e51c8ababe
Aleex1237 <alexis10893@outlook.com>
2022-03-03 10:15:22 -0300

    added error handler

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+
+const config = require('config');
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 mongoose.Promise = Promise;
 
-const express = require('express')
-const app = express()
 const indexRouter = require('./routes');
 
-const dotenv = require('dotenv');
-dotenv.config();
-const config = require('config');
 mongoose.connect('mongodb://' + config.get('mongodb.address') + '/' + config.get('mongodb.dbname'), { useNewUrlParser: true, useUnifiedTopology: true });
 require('./utils/initializer').init()
 
@@ -21,5 +21,14 @@ app.use('/api', indexRouter);
 // Start the server
 app.listen(config.get('port'));
 logger.info('API initialized on port ' + config.get('port'));
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      status: error.status || 500,
+      message: error.message || 'Internal Server Error.'
+    }
+  });
+});
 
 module.exports = app
