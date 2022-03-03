d1fced9a9b38356c4670218b916b9ae86c93b2dc
Aleex1237 <alexis10893@outlook.com>
2022-03-03 20:34:07 -0300

    added seeders on initializer for create 5 stores when the project run

--- utils/initializer.js
+++ utils/initializer.js
@@ -14,18 +14,52 @@ exports.init = async function () {
 
     logger.info("Test User created")
 
-    if (await Store.countDocuments({'name': 'storeTest'})) {
+    if (await Store.countDocuments() > 5) {
         return;
     }
-
-    let store = new Store();
-    store.name = 'storeTest';
-    store.cuit = '9029a9d';
-    store.concepts = ['store', 'almacen', 'tienda'];
-    store.currentBalance =  10000;
-    store.active =  true;
-    store.lastSale = Date.now();
-    await Store.create(store);
-
-    logger.info("Test Store created")
+    const stores = [
+        {
+        name : 'storeTest1',
+        cuit : '20951402',
+        concepts : ['store', 'almacen', 'tienda'],
+        currentBalance :  2000,
+        active :  true,
+        lastSale : Date.now()
+    },
+    {
+        name : 'storeTest2',
+        cuit : '20951445',
+        concepts : ['store', 'almacen', 'tienda'],
+        currentBalance :  20000,
+        active :  true,
+        lastSale : Date.now()
+    },
+    {
+        name : 'storeTest3',
+        cuit : '20951450',
+        concepts : ['store', 'almacen', 'tienda'],
+        currentBalance :  5000,
+        active :  true,
+        lastSale : Date.now()
+    },
+    {
+        name : 'storeTest4',
+        cuit : '20951124',
+        concepts : ['store', 'almacen', 'tienda'],
+        currentBalance :  300,
+        active :  true,
+        lastSale : Date.now()
+    },
+    {
+        name : 'storeTest5',
+        cuit : '20951423',
+        concepts : ['store', 'almacen', 'tienda'],
+        currentBalance :  250,
+        active :  true,
+        lastSale : Date.now()
+    },
+    ]
+    
+    await Store.insertMany(stores)
+    logger.info("Test Stores created")
 };
