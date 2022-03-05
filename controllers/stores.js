12482db497b5b06196e79970b971cc39f3e07ee0
Aleex1237 <alexis10893@outlook.com>
2022-03-05 13:39:10 -0300

    fixed json response

--- controllers/stores.js
+++ controllers/stores.js
@@ -1,39 +1,30 @@
-
 const storeService = require('../services/stores');
 const messages = require('../constants/messages');
 const statusCode = require('../constants/statusCodes');
-const logger = require('../utils/logger')
 
-const getAll = async (req,res,next) => {
+const getAll = async (req, res, next) => {
     try {
-        const {limit, skip} = req.query
-        const data = await storeService.getAll(limit, skip);
-    res.status(statusCode.RESPONSE_OK).json(
-    {   
-        status: statusCode.RESPONSE_OK,
-        msg: messages.RESPONSE_OK,
-        data
-    }
-    );
+        const { limit, page } = req.query
+        const data = await storeService.getAll(limit, page);
+
+        res.status(statusCode.RESPONSE_OK).json(data);
     } catch (error) {
         next(error);
     }
 
-    
+
 }
 
 const create = async (req, res, next) => {
     try {
         const store = await storeService.create(req.body);
 
-        logger.info("Store created.")
         res.status(statusCode.RESPONSE_OK_CREATED).json({
-            status:statusCode.RESPONSE_OK_CREATED,
-            msg:messages.RESPONSE_OK_CREATED,
+            status: statusCode.RESPONSE_OK_CREATED,
+            msg: messages.RESPONSE_OK_CREATED,
             store
         })
     } catch (error) {
-        logger.info("Error to create store")
         next(error);
     }
 }
