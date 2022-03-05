011e97119e036090b67ffe864048870a70cdb411
Aleex1237 <alexis10893@outlook.com>
2022-03-05 13:36:01 -0300

    app.js ordered

--- app.js
+++ app.js
@@ -2,25 +2,34 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 
-const config = require('config');
-const mongoose = require('mongoose');
+//Modules
 const logger = require('./utils/logger');
-mongoose.Promise = Promise;
+const morgan = require('morgan');
+const config = require('config');
+const db = require('./connection');
 
-const indexRouter = require('./routes');
+db.connection();
 
-mongoose.connect('mongodb://' + config.get('mongodb.address') + '/' + config.get('mongodb.dbname'), { useNewUrlParser: true, useUnifiedTopology: true });
+//Initializer
 require('./utils/initializer').init()
 
+//Routes
+const indexRouter = require('./routes');
+
+//Global Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 
+//Router Handler
 app.use('/api', indexRouter);
 
 // Start the server
 app.listen(config.get('port'));
 logger.info('API initialized on port ' + config.get('port'));
 
+
+//Error handler
 app.use((error, req, res, next) => {
   res.status(error.status || 500).json({
     error: {
