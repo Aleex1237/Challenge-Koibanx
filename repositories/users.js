9558d972107c039bf12683f228834f212e94fad2
Aleex1237 <alexis10893@outlook.com>
2022-03-05 13:42:19 -0300

    fixed db connection

--- repositories/users.js
+++ repositories/users.js
@@ -1,16 +1,13 @@
 const User = require('../models/user');
-require('../app');
+
 
 const getByEmail = async (email) => {
-
     const user = await User.find({ username: email });
-
     return user;
 }
 
 const getById = async (id) => {
     const user = await User.findById(id);
-
     return user;
 }
 
