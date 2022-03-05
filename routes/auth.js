002e5ba0defbcdece9b91462fd51910ea6b74cbe
Aleex1237 <alexis10893@outlook.com>
2022-03-05 13:37:19 -0300

    tiny refactor of routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 const authController = require('../controllers/auth');
 const authValidator = require('../validations/auth');
 
-router.post('/', authValidator.checkBody ,authController.login);
+router.post('/', authValidator.checkBody, authController.login)
 
 module.exports = router;
--- routes/stores.js
+++ routes/stores.js
@@ -1,16 +1,11 @@
-const logger = require('../utils/logger');
 const router = require('express').Router();
 
 const authHeader = require('../middlewares/auth');
-const store = require('../validations/store');
+const storeValidator = require('../validations/store');
 const storeControllers = require('../controllers/stores');
 
 router.get('/', authHeader.auth, storeControllers.getAll);
 
-router.post('/', store.checkBody, authHeader.auth, storeControllers.create);
-  
-
-/* router.route('/stores')
-  .get( logger.info("pending validations"), logger.info("pending use case"),  storeControllers.getAll); */
+router.post('/', storeValidator.checkBody, authHeader.auth, storeControllers.create);
 
 module.exports = router;
